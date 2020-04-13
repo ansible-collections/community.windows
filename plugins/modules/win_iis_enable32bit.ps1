58c21e2a7a0ffc0622f7bdf69214cf01766c2f85
Akchhay Alok <akchhay.alok91@gmail.com>
2020-04-13 12:01:07 +0530

    Added Enable 32 bit for pool

---
+++ plugins/modules/win_iis_enable32bit.ps1
@@ -0,0 +1,24 @@
+<#
+Function to enable 32 bit .net version for iis web Apps
+#>
+#Requires -Module WebAdministration
+Param(
+[Parameter(Mandatory=$true)][string]$poolname
+)
+<# Function to enable 32 bit for spesified pool 
+#>
+Function Enable32bitVersion
+    { 
+        [Cmdletbinding()]
+        try {
+            Import-Module WebAdministration
+            Set-ItemProperty IIS:\AppPools\$poolname -name "enable32BitAppOnWin64" -Value "true"
+            return "Enabled 32 Bit App for $poolname"
+        }
+        catch {
+        throw $_
+        }
+    }
+  
+Enable32bitVersion
+
