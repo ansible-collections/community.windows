4ea88f9fb5905b0fa8b0afaf15ccbf26d4e32889
Brian Scholer <1260690+briantist@users.noreply.github.com>
2020-04-29 19:43:26 -0400

    Fix style violation

--- plugins/action/win_pssession_configuration.py
+++ plugins/action/win_pssession_configuration.py
@@ -25,9 +25,9 @@ class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         self._supports_check_mode = True
         self._supports_async = False
-        check_mode      = self._play_context.check_mode
-        async_timeout   = self._task.args.get('async_timeout', self._default_async_timeout)
-        async_poll      = self._task.args.get('async_poll', self._default_async_poll)
+        check_mode = self._play_context.check_mode
+        async_timeout = self._task.args.get('async_timeout', self._default_async_timeout)
+        async_poll = self._task.args.get('async_poll', self._default_async_poll)
 
         result = super(ActionModule, self).run(tmp, task_vars)
 
