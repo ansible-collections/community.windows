151e85f78e43f9695af565b6f71464fa1e612239
sakarmehra100 <sakar.mehra@gslab.com>
2020-07-16 18:32:37 +0530

    Added updation code

--- plugins/modules/win_dns_record.ps1
+++ plugins/modules/win_dns_record.ps1
@@ -86,6 +86,22 @@ $changes = @{
 
 
 $records = Get-DnsServerResourceRecord -ZoneName $zone -Name $name -RRType $type -Node -ErrorAction:Ignore @extra_args | Sort-Object
+if($type -eq 'SRV' -and $null -ne $records){
+    $record_port_old = $records.RecordData.Port.ToString()
+    $record_priority_old = $records.RecordData.Priority.ToString()
+    $record_weight_old = $records.RecordData.Weight.ToString()
+    if($port -ne $record_port_old -or $priority -ne $record_priority_old -or $weight -ne $record_weight_old ){
+        $new_record = $records.Clone()
+        $new_record.RecordData.Port = $port
+        $new_record.RecordData.Priority = $priority
+        $new_record.RecordData.Weight = $weight
+
+        Set-DnsServerResourceRecord -ZoneName $zone -OldInputObject $records -NewInputObject $new_record -WhatIf:$module.CheckMode @extra_args
+        $changes.before += "[$zone] $($record.HostName) $($records.RecordData.Port) IN $type $record_port_old $record_weight_old $record_priority_old`n"
+        $changes.after += "[$zone] $($record.HostName) $($records.RecordData.Port) IN $type $port $weight $priority`n"
+        $module.Result.changed = $true
+    }
+}
 if ($null -ne $records) {
     # We use [Hashtable]$required_values below as a set rather than a map.
     # It provides quick lookup to test existing DNS record against. By removing
