20dedbb1e5f4fcb03cfc16d6a6334bc0f1998cb8
Tomoya Sakai <tomoya.sakai@shi-g.com>
2020-08-07 23:30:15 +0900

    win_net_adapter_feature.py not edited yet (only rename) win_net_adapter_feature.ps1 is under development

---
+++ plugins/modules/win_net_adapter_feature.ps1
@@ -0,0 +1,127 @@
+#!powershell
+#
+# (c) 2020, ライトウェルの人 <@jirolin>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+#
+# WANT_JSON
+# POWERSHELL_COMMON
+# temporarily disable strictmode, for this module only
+#Requires -Module Ansible.ModuleUtils.Legacy
+
+$ErrorActionPreference = "Stop"
+
+$Interface_name = [string[]]
+$componentID_name = [string[]]
+$params = Parse-Args $args -supports_check_mode $true
+#$Interface_name = Get-AnsibleParam -obj $params -name "Interface" -type "str"
+$Interface_name = Get-AnsibleParam -obj $params -name "Interface"
+$status = Get-AnsibleParam -obj $params -name status -type "str"
+#$componentID_name = Get-AnsibleParam -obj $params -name "componentID" -type "str"
+$componentID_name = Get-AnsibleParam -obj $params -name "componentID"
+$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+
+$result = @{
+    changed = $false
+}
+
+If(!$Interface_name) { Fail-Json -message "Absolute Interface is not specified for Interface" }
+If(!$status) { Fail-Json -message "Absolute status is not specified for status" }
+If(!$componentID_name) { Fail-Json -message "Absolute componentID is not specified for componentID" }
+
+If ($status -eq 'enable'){
+    $status = $true
+}ElseIf ($status -eq 'disable') {
+    $status = $false
+}Else {
+    Fail-Json -message "Specify the status as 'enable' or 'disable'"
+}
+
+If($Interface_name -is [string]) {
+    If($Interface_name.Length -gt 0) {
+        $Interfaces = @($Interface_name)
+    }
+    Else {
+        $Interfaces = @()
+    }
+}
+Else {
+        $Interfaces = $Interface_name
+}
+
+If($componentID_name -is [string]) {
+    If($componentID_name.Length -gt 0) {
+        $componentIDs = @($componentID_name)
+    }
+    Else {
+        $componentIDs = @()
+    }
+}
+Else {
+    $componentIDs = $componentID_name
+}
+
+Try {
+    If($Interface_name -eq "*") {
+        $Interfaces = Get-NetAdapter | Select-Object -ExpandProperty Name
+    }
+    else {
+        ForEach($Interface_name in $Interfaces) {
+        	If(@(Get-NetAdapter | Where-Object Name -eq $Interface_name).Count -eq 0){
+                Fail-Json -message "Invalid network adapter name: $Interface_name"
+            }
+        }
+    }
+
+    ForEach($componentID_name in $componentIDs) {
+	    If(@(Get-NetAdapterBinding | Where-Object ComponentID -eq $componentID_name).Count -eq 0) {
+	        Fail-Json -message "Invalid componentID: $componentID_name"
+        }
+    }
+
+    ForEach($componentID_name in $componentIDs) {
+        ForEach($Interface_name in $Interfaces) {
+            $current_status = (Get-NetAdapterBinding | where-object {$_.Name -match $Interface_name} | where-object {$_.ComponentID -match $componentID_name}).Enabled
+            $check_Idempotency = $true
+            If ($current_status -eq ""){
+                If ($current_status -eq $status){
+                    $check_Idempotency = $false
+                }
+            } ElseIf ($current_status -eq $status){
+                $check_Idempotency = $false
+            }
+
+            $result.changed = $result.changed -or $check_Idempotency
+
+            If($result.changed) {
+                If ($status -eq $true){
+                    Enable-NetAdapterBinding -Name $Interface_name -ComponentID $componentID_name -WhatIf:$check_mode
+                }Else {
+                    Disable-NetAdapterBinding -Name $Interface_name -ComponentID $componentID_name -WhatIf:$check_mode
+                }
+            }
+        }
+    }
+    Exit-Json $result
+}
+Catch {
+    $excep = $_
+
+    Fail-Json -message "Exception: $($excep | out-string)"
+
+    Throw
+}
+
