1d8c35d9ab520352dd50c4b50bc2c6ce4ab6a1ca
ライトウェルの人 <Jiro.Higuchi@shi-g.com>
2020-09-09 09:18:25 +0900

    Update plugins/modules/win_net_adapter_feature.py
    Co-authored-by: Jordan Borean <jborean93@gmail.com>

--- plugins/modules/win_net_adapter_feature.py
+++ plugins/modules/win_net_adapter_feature.py
@@ -4,8 +4,6 @@
 # Copyright: (c) 2020, ライトウェルの人 <jiro.higuchi@shi-g.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# this is a windows documentation stub.  actual code lives in the .ps1
-# file of the same name
 
 DOCUMENTATION = r'''
 ---
