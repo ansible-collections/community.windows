3ec5a145e3148874265d6945b7c2184471a597e1
Jordan Borean <jborean93@gmail.com>
2020-10-31 08:46:05 +1000

    Removed uneeded try/catch and simplify state check

--- plugins/modules/win_net_adapter_feature.ps1
+++ plugins/modules/win_net_adapter_feature.ps1
@@ -20,59 +20,46 @@ $state = $module.Params.state
 $component_id = $module.Params.component_id
 $check_mode = $module.CheckMode
 
-Try {
-    If($interface -eq "*") {
-        $interface = Get-NetAdapter | Select-Object -ExpandProperty Name
-    }Else {
-        ForEach($Interface_name in $interface) {
-            If(@(Get-NetAdapter | Where-Object Name -eq $Interface_name).Count -eq 0){
-                $module.FailJson("Invalid network adapter name: $Interface_name")
-            }
+If($interface -eq "*") {
+    $interface = Get-NetAdapter | Select-Object -ExpandProperty Name
+}Else {
+    ForEach($Interface_name in $interface) {
+        If(@(Get-NetAdapter | Where-Object Name -eq $Interface_name).Count -eq 0){
+            $module.FailJson("Invalid network adapter name: $Interface_name")
         }
     }
-
-    If($state -eq "enabled"){
-        $state = $true
-    }Else {
-        $state = $false
-    }
-
-    ForEach($componentID_name in $component_id) {
-	    If(@(Get-NetAdapterBinding | Where-Object ComponentID -eq $componentID_name).Count -eq 0) {
-	        $module.FailJson("Invalid componentID: $componentID_name")
-        }
-    }
-
-    $module.Result.changed = $false
-
-    ForEach($componentID_name in $component_id) {
-        ForEach($Interface_name in $interface) {
-            $current_state = (Get-NetAdapterBinding | where-object {$_.Name -eq $Interface_name} | where-object {$_.ComponentID -eq $componentID_name}).Enabled
-            #Initialize the check_Idempotency flag for each interface, and for each component_id.
-            $check_Idempotency = $true
-
-            If ($current_state -eq $state){
-                $check_Idempotency = $false
-            }
-
-            #Even Once $check_Idempotency remains $true, $module.Result.changed turns $true.
-            $module.Result.changed = $module.Result.changed -Or $check_Idempotency
-
-            If($check_Idempotency) {
-                If ($state -eq "True"){
-                    Enable-NetAdapterBinding -Name $Interface_name -ComponentID $componentID_name -WhatIf:$check_mode
-                }Else {
-                    Disable-NetAdapterBinding -Name $Interface_name -ComponentID $componentID_name -WhatIf:$check_mode
-                }
-            }
-        }
-    }
-    $module.ExitJson()
 }
-Catch {
-    $excep = $_
 
-    $module.FailJson("Exception: $($excep | out-string)")
+$state = $state -eq "enabled"
 
-    Throw
+ForEach($componentID_name in $component_id) {
+    If(@(Get-NetAdapterBinding | Where-Object ComponentID -eq $componentID_name).Count -eq 0) {
+        $module.FailJson("Invalid componentID: $componentID_name")
+    }
 }
+
+$module.Result.changed = $false
+
+ForEach($componentID_name in $component_id) {
+    ForEach($Interface_name in $interface) {
+        $current_state = (Get-NetAdapterBinding | where-object {$_.Name -eq $Interface_name} | where-object {$_.ComponentID -eq $componentID_name}).Enabled
+        #Initialize the check_Idempotency flag for each interface, and for each component_id.
+        $check_Idempotency = $true
+
+        If ($current_state -eq $state){
+            $check_Idempotency = $false
+        }
+
+        #Even Once $check_Idempotency remains $true, $module.Result.changed turns $true.
+        $module.Result.changed = $module.Result.changed -Or $check_Idempotency
+
+        If($check_Idempotency) {
+            If ($state -eq "True"){
+                Enable-NetAdapterBinding -Name $Interface_name -ComponentID $componentID_name -WhatIf:$check_mode
+            }Else {
+                Disable-NetAdapterBinding -Name $Interface_name -ComponentID $componentID_name -WhatIf:$check_mode
+            }
+        }
+    }
+}
+$module.ExitJson()
