5966a07d8563e90ae64ccf0d5969bea6b95efcb6
sgruber <sgruber@cbc-ag.de>
2021-02-17 21:08:21 +0100

    changed test, doc, module

--- plugins/modules/win_gpo_link.ps1
+++ plugins/modules/win_gpo_link.ps1
@@ -1,10 +1,9 @@
 #!powershell
-# Copyright: (c) 2020 Sebastian Gruber ,dacoso GmbH All Rights Reserved.
+# Copyright: (c) 2021 Sebastian Gruber ,dacoso GmbH All Rights Reserved.
 # SPDX-License-Identifier: GPL-3.0-only
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # AnsibleRequires -CSharpUtil Ansible.Basic
 
-$log_path = c:\temp\meinlog.txt
 try {
     Import-Module GroupPolicy
 } catch {
@@ -15,31 +14,17 @@ try {
 } catch {
     $module.FailJson("win_grouppolicy requires the activedirectory  PS module to be installed")
 }
-Function Write-DebugLog {
-    Param(
-        [string]$msg
-    )
 
-    $DebugPreference = "Continue"
-    $ErrorActionPreference = "Continue"
-    $date_str = Get-Date -Format u
-    $msg = "$date_str $msg"
-
-    Write-Debug $msg
-    if($log_path) {
-        Add-Content $log_path $msg
-    }
-}
 function Get-Gplink {
 
-    <# 
+    <#
     .SYNOPSIS
     return and decode content of gplink attribut
-    
+
     .DESCRIPTION
     This function purpose is to list the gplink attribut of an OU, Site or DomainDNS.
     It will return the following information for each linked GPOs:
-    
+
         - Target: DN of the targeted object
         - GPOID: GUID of the GPO
         - GPOName: Friendly Name of the GPO
@@ -48,13 +33,13 @@ function Get-Gplink {
         - Enabled:  <Yes|No>
         - Order: Link order of the GPO on the OU,Site,DomainDNS (does not report inherited order)
         #author https://gallery.technet.microsoft.com/scriptcenter/Get-GPlink-Function-V13-b31253b4
-    
+
     .PARAMETER Path: Give de Distinguished Name of object you want to list the gplink
-    
-    
+
+
     .INPUTS
     DN of the object with GLINK attribut
-    
+
     .OUTPUTS
     Target: DC=fourthcoffee,DC=com
     GPOID: 31B2F340-016D-11D2-945F-00C04FB984F9
@@ -63,47 +48,47 @@ function Get-Gplink {
     Enforced: <YES - NO>
     Enabled: <YES - NO>
     Order: 1
-    
+
     .EXAMPLE
     get-gplink -path "dc=fourthcoffee,dc=com"
-    
-    This command will list the GPOs that are linked to the DomainDNS object "dc=fourthcoffee,dc=com" 
-    
+
+    This command will list the GPOs that are linked to the DomainDNS object "dc=fourthcoffee,dc=com"
+
     .EXAMPLE
     get-gplink -path "dc=child,dc=fourthcoffee,dc=com" -server childdc.child.fourthcoffee.com
-    
+
     This command will list the GPOs that are linked to the DomainDNS object "dc=child,dc=fourthcoffee,dc=com". You need to specify a
     target DC of the domain child.fourthcoffee.com in order for the command to work.
-    
-    
+
+
     .EXAMPLE
     Get-Gplink -site "CN=Default-First-Site-Name,CN=Sites,CN=Configuration,DC=fourthcoffee,DC=com"
-    
+
     This command will list the GPOs that are linked to site "Default-First-Site-Name"
-    
+
     .EXAMPLE
     get-gplink -path "dc=fourthcoffee,dc=com" | export-csv "gplink.csv"
-    
+
     This command will list the GPOs that are linked to the DomainDNS object "dc=fourthcoffee,dc=com" and export them to a csv file.
-    The csv file can be used as an input to the cmdlet new-gplink 
-    
-    
+    The csv file can be used as an input to the cmdlet new-gplink
+
+
     .EXAMPLE
     get-adobject -filter {(objectclass -eq "DomainDNS") -or (objectclass -eq "OrganizationalUnit")} | foreach {get-gplink -path $_.distinguishedname} | export-csv "gplinksall.csv"
-    
+
     This command will list all objects of type "DomainDNS" and "OrganizationalUnit" that have GPOs linked and will list those GPOs, their status and link order.
-    
+
     #>
-    
+
     [cmdletBinding()]
     param ([string]$path, [string]$server, [string]$site)
-    
+
     #Import AD and GPO modules
     Import-Module activedirectory
     Import-Module grouppolicy
     # get the DN to te configuration partition
     $configpart = (Get-ADRootDSE).configurationNamingContext
-    
+
     #get content of attribut gplink on site object or OU
     if ($site) {
         $gplink = Get-ADObject -Filter { distinguishedname -eq $site } -searchbase $configpart -Properties gplink
@@ -112,19 +97,19 @@ function Get-Gplink {
         switch ($server) {
             "" { $gplink = Get-ADObject -Filter { distinguishedname -eq $path } -Properties gplink }
             default {
-                $gplink = Get-ADObject -Filter { distinguishedname -eq $path } -Properties gplink -server $server     
+                $gplink = Get-ADObject -Filter { distinguishedname -eq $path } -Properties gplink -server $server
             }
         }
         $target = $path
     }
     #if DN is not valid return" Invalide DN" error
     if ($null -eq $gplink) {
-        Write-DebugLog "Either Invalide DN in the current domain, specify a DC of the target DN domain or no GPOlinked to this DN"
+        $module.FailJson("Either Invalide DN in the current domain, specify a DC of the target DN domain or no GPOlinked to this DN")
     }
-    # test if glink is not null or only containes white space before continuing. 
+    # test if glink is not null or only containes white space before continuing.
     if (!((($gplink.gplink) -like "") -or (($gplink.gplink) -like " "))) {
         #set variale $o to define link order
-        $o = 0    
+        $o = 0
         #we split the gplink string in order to seperate the diffent GPO linked
         $split = $gplink.gplink.split("]")
         #we need to do a reverse for to get the proper link order
@@ -132,20 +117,19 @@ function Get-Gplink {
             #since the last character in the gplink string is a "]" the last split is empty we need to ignore it
             if ($split[$s].length -gt 0) {
                 $o++
-                $order = $o            
+                $order = $o
                 $gpoguid = $split[$s].substring(12, 36)
                 $gpodomainDN = ($split[$s].substring(72)).split(";")
                 $domain = ($gpodomaindn[0].substring(3)).replace(",DC=", ".")
-                $checkdc = (get-addomaincontroller -domainname $domain -discover).name                              
-                #we test if the $gpoguid is a valid GUID in the domain if not we return a "Oprhaned GpLink or External GPO" in the $gponname               
-                $mygpo = get-gpo -guid $gpoguid -domain $domain -server "$($checkdc).$($domain)" 2> $null
+                $checkdc = (get-addomaincontroller -domainname $domain -discover).name
+                #we test if the $gpoguid is a valid GUID in the domain if not we return a "Oprhaned GpLink or External GPO" in the $gponname
+                $mygpo = get-gpo -guid $gpoguid -domain $domain -server "$($checkdc)$($domain)2> $null"
                 if ($null -ne $mygpo ) {
                     $gponame = $MyGPO.displayname
-                    $gpodomain = $domain	
-                }              
-                else {
-                    $gponame = "Orphaned GPLink" 
-                    $gpodomain = $domain   
+                    $gpodomain = $domain
+                } else {
+                    $gponame = "Orphaned GPLink"
+                    $gpodomain = $domain
                 }
                 #we test the last 2 charaters of the split do determine the status of the GPO link
                 if (($split[$s].endswith(";0"))) {
@@ -162,8 +146,8 @@ function Get-Gplink {
                     $enforced = "Yes"
                 }
                 #we create an object representing each GPOs, its links status and link order
-                $return = New-Object psobject 
-                $return | Add-Member -membertype NoteProperty -Name "Target" -Value $target 
+                $return = New-Object psobject
+                $return | Add-Member -membertype NoteProperty -Name "Target" -Value $target
                 $return | Add-Member -membertype NoteProperty -Name "GPOID" -Value $gpoguid
                 $return | Add-Member -membertype NoteProperty -Name "DisplayName" -Value $gponame
                 $return | Add-Member -membertype NoteProperty -Name "Domain" -Value $gpodomain
@@ -178,13 +162,12 @@ function Get-Gplink {
 $spec = @{
     options = @{
         state = @{ type = "str"; choices = "query", "present", "absent"; default = "present" }
-        path = @{ type = "list"; elements = "str"}
-        gponame = @{ type = "str"; required = $true}
+        path = @{ type = "list"; elements = "str" }
+        gponame = @{ type = "str"; required = $true }
         order = @{ type = "int" }
         domain = @{ type = "str" }
-        log_path = @{ type = "str" }
-        enforced = @{ type = "bool"; default = $false}
-        linkenabled = @{  type = "bool"; default = $true}
+        enforced = @{ type = "bool"; default = $false }
+        linkenabled = @{  type = "bool"; default = $true }
     }
     supports_check_mode = $true
 }
@@ -197,23 +180,22 @@ $gpodomain = $module.Params.domain
 $gposerver = $module.Params.server
 $enforcedbool = $module.Params.enforced
 $gpolinkenabledbool = $module.Params.linkenabled
-$global:log_path = $log_path = $module.Params.log_path
 $ErrorActionPreference = 'Stop'#Error Action
 
-#Converting Needed Variables from bool into Strings :) 
+#Converting Needed Variables from bool into Strings :)
 #Microsoft specified that as string. Possible "No","yes","Unspecified" https://docs.microsoft.com/en-us/powershell/module/grouppolicy/set-gplink
-if($gpolinkenabledbool) {
+if ($gpolinkenabledbool) {
     $gpolinkenabled = "yes"
 } else {
     $gpolinkenabled = "no"
 }
-if($enforcedbool) {
+if ($enforcedbool) {
     $enforced = "yes"
 } else {
     $enforced = "no"
 }
 #States
-if($state -eq "query") {
+if ($state -eq "query") {
     foreach ($pa in $path) {
         [string]$path = $pa
         if ($path) {
@@ -223,18 +205,17 @@ if($state -eq "query") {
         }
     }
 }
-if($state -eq "present") {
+if ($state -eq "present") {
     $gpos = Get-GPO -All
-    if($gpos.Displayname -contains $gponame) {
+    if ($gpos.Displayname -contains $gponame) {
             #handle each path
-            foreach($pa in $path)
-            {
+            foreach ($pa in $path) {
                 [string]$path = $pa
                 if ($path) {
                     #collect all Informations about GPO
                     $gpodefault = @{
-                        Name     = $gponame
-                        Target   = $path
+                        Name = $gponame
+                        Target = $path
                         Enforced = $enforced
                     }
                     #Settings for Settings like "order"
@@ -246,19 +227,17 @@ if($state -eq "present") {
                     if ($gposerver) { $gposetparam.Server = $gposerver }
                     if ($checkmode) { $gposetparam.WhatIf = $true }
                     $gpoalllinks = get-gplink -path $path #get all links for specific path
-                    $gpoonpath = $gpoalllinks | Where-Object { $_.DisplayName -match $gponame } | Select-Object * #filter information for specific 
-                    if( $gpoalllinks.DisplayName -notcontains $gponame) {
+                    $gpoonpath = $gpoalllinks | Where-Object { $_.DisplayName -match $gponame } | Select-Object * #filter information for specific
+                    if ( $gpoalllinks.DisplayName -notcontains $gponame) {
                         #gpo link not exists create link
-                        Write-DebugLog "creating GPO Link"
                         new-gplink -name $gponame -Target $path
                         $module.result.changed = $true
                     }
-                    if(($gpoonpath.Enforced -match $gposetparam.Enforced) -and ($gpoonpath.Enabled -match $gposetparam.LinkEnabled) -and (($null -eq $gposetparam.order) -or ($gpoonpath.Order -eq $gposetparam.order))) {
+                    if (($gpoonpath.Enforced -match $gposetparam.Enforced) -and ($gpoonpath.Enabled -match $gposetparam.LinkEnabled) -and (($null -eq $gposetparam.order) -or ($gpoonpath.Order -eq $gposetparam.order))) {
                         # nogplink  update needed
                         $module.result.changed = $false
                     } else {
                         #gplink needs update
-                        Write-DebugLog "changing GPO Link"
                         Set-GPLink @gpodefault @gposetparam
                         $module.result.changed = $true
                     }
@@ -272,17 +251,15 @@ if($state -eq "present") {
         $module.FailJson("GPO not exist")
     }
 }
-if($state -eq "absent") {
-    foreach($pa in $path)
-    {
+if ($state -eq "absent") {
+    foreach ($pa in $path) {
         [string]$path = $pa
         if ($path) {
             $gpos = Get-GPO -All
-            if($gpos.Displayname -contains $gponame) {
+            if ($gpos.Displayname -contains $gponame) {
                 # gpo exists in AD
                 $gpoonpath = get-gplink -path $path #get all links for specific path
-                if($gpoonpath.DisplayName -contains $gponame ) {
-                    Write-DebugLog "Remove GPO Link"
+                if ($gpoonpath.DisplayName -contains $gponame ) {
                     Remove-gplink -Name $gponame -Target $path
                     $module.result.changed = $true
                 } else {
--- plugins/modules/win_gpo_link.py
+++ plugins/modules/win_gpo_link.py
@@ -1,75 +1,78 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright: (c) 2020 Sebastian Gruber ,dacoso GmbH All Rights Reserved.
+# Copyright: (c) 2021 Sebastian Gruber ,dacoso GmbH All Rights Reserved.
 # SPDX-License-Identifier: GPL-3.0-only
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 DOCUMENTATION = r'''
 ---
 module: win_gpo_link
+author: Sebastian Gruber (@sgruber94)
 short_description: Link a GroupPolicy to a specific ActiveDirectory OU
 description:
 - The C(win_gpo_link) module can link GroupPolicy to a specific ActiveDirectory OU.
-options:
-state:
-    description:
-    - Specify State 
-    type:str
-    choices: [ present, absent, query ]
-    required: yes
-    default: present
-path:
-    description:
-    - Specifies OU for GPO Link
-    type:str
-    required: yes
-gponame:
-    description:
-    - used by module when creating,quering or removing GPO Link
-    - Name of the  GPO
-    type:str
-    required: yes
-enforced:
-    description:
-    - That the settings of the GPO always take precedence over conflicting settings in GPOs that are linked to lower-level containers.
-    - That the settings of the GPO cannot be blocked (by blocking inheritance) at a lower-level Active Directory container.
-    type:bool
-    Default: no
-linkenabled:
-    description:
-    - Disabling a GPO link does not disable the GPO itself
-    type:bool
-    Default: yes
-order:
-    description:
-    - Specify the order number of specific GPO
-    type:int
-log_path:
-    description:
-    - Specify log file for Debug purposes
+requirements:
+  - PowerShell Module GroupPolicy (https://docs.microsoft.com/en-us/powershell/module/grouppolicy)
+  - PowerShell Module activedirectory (https://docs.microsoft.com/en-us/powershell/module/activedirectory)
+seealso:
+  - module: community.windows.win_gpo_force
+  - module: community.windows.win_gpo_manage
 notes:
 - This must be run on a host that has the GroupPolicy PowerShell module installed.
+options:
+  state:
+    description:
+      - Specify State
+    type: str
+    choices: [ present, absent, query ]
+    default: present
+  path:
+    description:
+      - Specifies OU for GPO Link
+    type: str
+    required: yes
+  gponame:
+    description:
+      - used by module when creating,quering or removing GPO Link
+      - Name of the  GPO
+    type: str
+    required: yes
+  enforced:
+    description:
+      - That the settings of the GPO always take precedence over conflicting settings in GPOs that are linked to lower-level containers.
+      - That the settings of the GPO cannot be blocked (by blocking inheritance) at a lower-level Active Directory container.
+    type: bool
+    default: no
+  linkenabled:
+    description:
+      - Disabling a GPO link does not disable the GPO itself
+    type: bool
+    default: yes
+  order:
+    description:
+      - Specify the order number of specific GPO
+    type: int
 '''
 
 EXAMPLES = r'''
 
 - name: Create simple GroupPolicy Link
-    win_gpo_link:
-      state: present
-      gponame: mysimpletest
-      path: 'OU=Switzerland,OU=Lab Accounts,DC=intern,DC=systemuser,DC=de'
+  community.windows.win_gpo_link:
+    state: present
+    gponame: mysimpletest
+    path: 'OU=Switzerland,OU=Lab Accounts,DC=intern,DC=example,DC=de'
 
 - name: Create GroupPolicy Link enforced
-    win_gpo_link:
-      state: present
-      gponame: mysimpletestmysimpletestenforced
-      path: 'OU=Switzerland,OU=Lab Accounts,DC=intern,DC=systemuser,DC=de'
-      enforced: yes
+  community.windows.win_gpo_link:
+    state: present
+    gponame: mysimpletestmysimpletestenforced
+    path: 'OU=Switzerland,OU=Lab Accounts,DC=intern,DC=example,DC=de'
+    enforced: yes
 
 - name: Delete GroupPolicy Link enforced
-    win_gpo_link:
-        state: absent
-        gponame: test99
-        path: 'OU=Switzerland,OU=Lab Accounts,DC=intern,DC=systemuser,DC=de'
+  community.windows.win_gpo_link:
+    state: absent
+    gponame: test99
+    path: 'OU=Switzerland,OU=Lab Accounts,DC=intern,DC=example,DC=de'
 
 '''
