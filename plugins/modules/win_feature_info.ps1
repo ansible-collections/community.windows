49863aa766ec511176f9622046ab9ad8d752abaa
Larry <34108925+gamethis@users.noreply.github.com>
2021-03-12 08:38:03 -0600

    added gnu back

--- plugins/modules/win_feature_info.ps1
+++ plugins/modules/win_feature_info.ps1
@@ -1,5 +1,7 @@
 #!powershell
 
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 $spec = @{
     options             = @{
         name = @{ type = "str"; default = '*'  }
