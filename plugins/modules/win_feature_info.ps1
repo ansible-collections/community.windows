219ef6d70f093d9ff5c93114adfffda3a5ed4e20
Larry <34108925+gamethis@users.noreply.github.com>
2021-03-12 08:47:59 -0600

    put proper basic import in

--- plugins/modules/win_feature_info.ps1
+++ plugins/modules/win_feature_info.ps1
@@ -1,6 +1,7 @@
 #!powershell
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#AnsibleRequires -CSharpUtil Ansible.Basic
 
 $spec = @{
     options             = @{
