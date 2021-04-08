2e551d8f590b9c9d9b209bff406985427b2da6d9
sgruber <12892285+sgruber94@users.noreply.github.com>
2021-04-08 14:22:49 +0200

    requested changes

--- plugins/modules/win_dhcp_option.ps1
+++ plugins/modules/win_dhcp_option.ps1
@@ -6,20 +6,20 @@
 
 $spec = @{
     options             = @{
-        type             = @{ type = "str"; choices = "server", "scope", "reservation"; required = $true }
-        version          = @{ type = "str"; choices = "IPv4", "IPv6"; default = "IPv4" }
-        state            = @{ type = "str"; choices = "absent", "present"; default = "present" }
-        scope            = @{ type = "str"; aliases = "prefix" }
-        reservedip       = @{ type = "str" }
-        optionid         = @{ type = "int" }
-        value            = @{ type = "str" }
-        VendorClass      = @{ type = "str" }
-        computername     = @{ type = "str" }
-        dnsserver        = @{ type = "str" }
-        dnsdomain        = @{ type = "str" } #ipv4 only
-        router           = @{ type = "str" } #ipv4 only
-        domainsearchlist = @{ type = "str" }
-        force            = @{ type = "bool"; default = $false }
+        type               = @{ type = "str"; choices = "server", "scope", "reservation"; required = $true }
+        version            = @{ type = "str"; choices = "IPv4", "IPv6"; default = "IPv4" }
+        state              = @{ type = "str"; choices = "absent", "present"; default = "present" }
+        scope              = @{ type = "str"; aliases = "prefix" }
+        reserved_ip        = @{ type = "str" }
+        option_id          = @{ type = "int" }
+        value              = @{ type = "str" }
+        vendor_class       = @{ type = "str" }
+        computer_name      = @{ type = "str" }
+        dns_server         = @{ type = "str" }
+        dns_domain         = @{ type = "str" } #ipv4 only
+        router             = @{ type = "str" } #ipv4 only
+        domain_search_list = @{ type = "str" }
+        force              = @{ type = "bool"; default = $false }
     }
     supports_check_mode = $true
 }
@@ -28,45 +28,40 @@ $check_mode = $module.CheckMode
 $type = $module.Params.type
 $version = $module.Params.version
 $scope_id = $module.Params.scope
-[System.UInt32]$optionid = $module.Params.optionid
+[System.UInt32]$optionid = $module.Params.option_id
 $value = $module.Params.value
-$domainsearchlist = $module.Params.domainsearchlist
+$domainsearchlist = $module.Params.domain_search_list
 
 #extraparam
 $state = $module.Params.state
-$reservedip = $module.Params.reservedip
-$computername = $module.Params.computername #optional to specifiy another computer
-$dnsserver = $module.Params.dnsserver #optional to specifiy dns server
-$dnsdomain = $module.Params.dnsdomain
+$reservedip = $module.Params.reserved_ip
+$computername = $module.Params.computer_name #optional to specifiy another computer
+$dnsserver = $module.Params.dns_server #optional to specifiy dns server
+$dnsdomain = $module.Params.dns_domain
 $router = $module.Params.router
 $force = $module.Params.force #optional for forcing Parameter
-Try {
-    # Import DHCP Server PS Module
-    Import-Module DhcpServer
-} Catch {
-    # Couldn't load the DhcpServer Module
-    $module.FailJson("The DhcpServer module failed to load properly: $($_.Exception.Message)", $_)
-}
+
+Import-Module DhcpServer
 $extra_param = @{}
 $get_param = @{}
-if($computername) { $extra_param.ComputerName = $computername }
-if($computername) { $get_param.ComputerName = $computername }
-if(($type -eq "scope") -and ($version -eq "IPv4") ) {
+if ($computername) { $extra_param.ComputerName = $computername }
+if ($computername) { $get_param.ComputerName = $computername }
+if (($type -eq "scope") -and ($version -eq "IPv4") ) {
     $extra_param.Scope = $scope_id
     $get_param.Scope = $scope_id
 }
-if(($type -eq "scope") -and ($version -eq "IPv6") ) {
+if (($type -eq "scope") -and ($version -eq "IPv6") ) {
     $extra_param.Prefix = $scope_id
     $get_param.Prefix = $scope_id
 }
 
-if($type -eq "reservation") {
+if ($type -eq "reservation") {
     $get_param.ReservedIP = $reservedip
     $extra_param.ReservedIP = $reservedip
 }
-if($dnsserver) { $extra_param.DnsServer = $dnsserver }
+if ($dnsserver) { $extra_param.DnsServer = $dnsserver }
 
-if($state -eq "present") {
+if ($state -eq "present") {
     if ($version -eq "IPv4") {
         $alldhcpoptions = Get-DhcpServerv4OptionValue @get_param
     } else {
@@ -74,19 +69,19 @@ if($state -eq "present") {
         $alldhcpoptions = Get-DhcpServerv6OptionValue @get_param
     }
     $current_release = $alldhcpoptions | Where-Object { $_.OptionId -eq $optionid } | Select-Object *
-    if(((($current_release.value -eq $value) -and ($current_release.OptionId -eq $optionid)) -or (($current_release.VendorClass -eq $VendorClass) -and ($null -ne $current_release.VendorClass) ))) {
+    if (((($current_release.value -eq $value) -and ($current_release.OptionId -eq $optionid)) -or (($current_release.VendorClass -eq $VendorClass) -and ($null -ne $current_release.VendorClass) ))) {
         #nothing to do here
         $module.result.changed = $false
     } else {
-        if($check_mode) { $extra_param.WhatIf = $check_mode }
-        if($force) { $extra_param.Force = $force }
-        if($domainsearchlist) { $extra_param.DomainSearchList = $domainsearchlist }
-        if($optionid) { $extra_param.OptionId = $optionid }
-        if($Value) { $extra_param.Value = $Value }
+        if ($check_mode) { $extra_param.WhatIf = $check_mode }
+        if ($force) { $extra_param.Force = $force }
+        if ($domainsearchlist) { $extra_param.DomainSearchList = $domainsearchlist }
+        if ($optionid) { $extra_param.OptionId = $optionid }
+        if ($Value) { $extra_param.Value = $Value }
         #options not set or not equal
-        if($version -eq "IPv4") {
-            if($dnsdomain) { $extra_param.dnsdomain = $dnsdomain }
-            if($router) { $extra_param.router = $router }
+        if ($version -eq "IPv4") {
+            if ($dnsdomain) { $extra_param.dnsdomain = $dnsdomain }
+            if ($router) { $extra_param.router = $router }
             try {
                 Set-DhcpServerv4OptionValue @extra_param
                 $module.result.changed = $true
@@ -113,39 +108,30 @@ if ($state -eq "absent") {
         $alldhcpoptions = Get-DhcpServerv6OptionValue @extra_param
     }
     $current_release = $alldhcpoptions | Where-Object { $_.OptionId -eq $optionid } | Select-Object *
-    if(((($current_release.value -eq $value) -and ($current_release.OptionId -eq $optionid)) -or (($current_release.VendorClass -eq $VendorClass) -and ($null -ne $current_release.VendorClass) ))){
+    if (((($current_release.value -eq $value) -and ($current_release.OptionId -eq $optionid)) -or (($current_release.VendorClass -eq $VendorClass) -and ($null -ne $current_release.VendorClass) ))) {
         #nothing to do here
         $changes = $false
     } else {
         $changes = $true
     }
-    if($changes) {
-        if($check_mode) { $extra_param.WhatIf = $check_mode }
-        if($force) { $extra_param.Force = $force }
-        if($domainsearchlist) { $extra_param.DomainSearchList = $domainsearchlist }
+    if ($changes) {
+        if ($check_mode) { $extra_param.WhatIf = $check_mode }
+        if ($force) { $extra_param.Force = $force }
+        if ($domainsearchlist) { $extra_param.DomainSearchList = $domainsearchlist }
         #options not set or not equal
-        if($version -eq "IPv4") {
-            if($dnsdomain) { $extra_param.dnsdomain = $dnsdomain }
-            if($router) { $extra_param.router = $router }
-            try {
-                Set-DhcpServerv4OptionValue @extra_param
-                $module.result.changed = $true
-            } catch {
-                $module.FailJson("unexpected error while adding DHCP Option : $($_.Exception.Message)", $_)
-            }
+        if ($version -eq "IPv4") {
+            if ($dnsdomain) { $extra_param.dnsdomain = $dnsdomain }
+            if ($router) { $extra_param.router = $router }
+            Set-DhcpServerv4OptionValue @extra_param
+            $module.result.changed = $true
         } else {
             #IPv6
-            try {
-                Set-DhcpServerv6OptionValue @extra_param
-                $module.result.changed = $true
-            } catch {
-                $module.FailJson("unexpected error while removing DHCP Option : $($_.Exception.Message)", $_)
-            }
+            Set-DhcpServerv6OptionValue @extra_param
+            $module.result.changed = $true
         }
     } else {
         $module.result.changed = $false
         #noting to do here
     }
 }
-#End of Script
 $module.ExitJson()
--- plugins/modules/win_dhcp_option.py
+++ plugins/modules/win_dhcp_option.py
@@ -9,6 +9,7 @@ DOCUMENTATION = r'''
 module: win_dhcp_option
 short_description: Manage Windows Server DHCP Options
 author: Sebastian Gruber (@sgruber94)
+version_added: '1.4.0'
 requirements:
   - This module requires Windows Server 2012 or Newer
 description:
@@ -45,12 +46,12 @@ options:
     - Prefix will be used for IPv6
     type: str
     aliases: [prefix]
-  reservedip:
+  reserved_ip:
     description:
     - Specifies the IPv4/IPv6 address of the reservation for which the option value are set.
     - Required if l(type=reservation), otherwise ignored.
     type: str
-  optionid:
+  option_id:
     description:
     - Specifies the numeric identifier (ID) of the option for which the value are set.
     - Required if l(type=reservation), otherwise ignored.
@@ -59,7 +60,7 @@ options:
     description:
     - Specifies one value to be set for the option.
     type: str
-  dnsdomain:
+  dns_domain:
     description:
     - Specifies one or more values for the domain search list option.
     type: str
@@ -68,19 +69,19 @@ options:
     - IPv4 only
     - Specifies one or more values for the router or default gateway option, in IPv4 address format.
     type: str
-  VendorClass:
+  vendor_class:
     description:
     - Sets the option value for the specified vendor class
     type: str
-  dnsserver:
+  dns_server:
     description:
     - optional for specify DNS Server
     type: str
-  computername:
+  computer_name:
     description:
     - optional for specify another Computer
     type: str
-  domainsearchlist:
+  domain_search_list:
     description:
     - optional for specify another Computer
     type: str
@@ -93,30 +94,30 @@ options:
 
 EXAMPLES = r'''
     - name: Add IPv6 Server Option
-      win_dhcp_option:
+      community.windows.win_dhcp_option:
           state: present
           version: "IPv6"
           type: "server"
-          optionid: 24
+          option_id: 24
           value: "server.ipv6.intern.example.de"
 
     - name: Add IPv6 Scope Option
-      win_dhcp_option:
+      community.windows.win_dhcp_option:
           state: present
           version: "IPv6"
           type: "scope"
-          optionid: 24
+          option_id: 24
           value: "scope.ipv6.intern.example.de"
           prefix: "2001:5cc1::"
 
     - name: Add IPv4 Reservation Option
-      win_dhcp_option:
+      community.windows.win_dhcp_option:
           state: present
           version: "IPv4"
           type: "reservation"
-          optionid: 15
+          option_id: 15
           value: "reserved.ipv4.intern.example.de"
-          reservedip: "192.168.222.222"
+          reserved_ip: "192.168.222.222"
 '''
 
 RETURN = r'''
