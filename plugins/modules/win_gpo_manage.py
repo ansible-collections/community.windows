52e856b60b2b0b0db3be9a8e5eeb21c24ad26e0e
sgruber <12892285+sgruber94@users.noreply.github.com>
2021-04-08 15:10:42 +0200

    requested changes

--- plugins/modules/win_gpo_manage.py
+++ plugins/modules/win_gpo_manage.py
@@ -9,43 +9,39 @@ DOCUMENTATION = r'''
 module: win_gpo_manage
 short_description: Imports,Export,Removes Group Policy on windows hosts
 author: Sebastian Gruber (@sgruber94)
+version_added: '1.4.0'
 description:
   - The C(win_gpo_manage) module can manage GroupPolicys on the windows host.
 requirements:
   - PowerShell Module GroupPolicy (https://docs.microsoft.com/en-us/powershell/module/grouppolicy)
-seealso:
-  - module: community.windows.win_gpo_force
-  - module: community.windows.win_gpo_link
 options:
   mode:
     description:
-      - When l(type=import), GroupPolicy will be imported from a specific Folder
-      - When l(type=export), GroupPolicy will be exported from to a specific Folder
-      - When l(type=query), GroupPolicy will be query
-      - When l(type=remove), is set then I(name) must also be set. Deletes GPO
+      - When l(type=present), GroupPolicy will be imported from a specific Folder
+      - When l(type=absent), is set then I(name) must also be set. Deletes GPO
+      - When l(type=exported), GroupPolicy will be exported from to a specific Folder
     type: str
     choices:
-      - import
-      - export
-      - query
-      - remove
+      - present
+      - absent
+      - exported
     required: yes
   folder:
     description:
-      - Required if l(type=import) or l(type=export)
+      - Required if l(type=present) or l(type=export)
       - Specify Folder to Import or Export GroupPolicy
     type: path
     default: C:\GPO
   name:
     description:
-      - Required if l(type=remove) is set.
+      - Required if l(type=absent) is set.
       - Specify Name of GroupPolicies that should be deleted
     elements: str
     type: list
   override:
     description:
-      - if override is set to yes, in mode import all Policy will be overriden
-      - if override is set to yes , in mode export, all Policy that are duplicates will be deleted and with the new export replaced
+      - if override is set to yes, in mode present all Policy will be overriden
+      - if override is set to yes , in mode exported, all Policy that are duplicates will be deleted and with the new export replaced
     type: bool
     default: no
 '''
@@ -53,22 +49,18 @@ options:
 EXAMPLES = r'''
 - name: Import GroupPolicys from a Folder
   community.windows.win_gpo_manage:
-    mode: import
+    mode: present
     folder: C:\gpo
 
 - name: Export all GroupPolicys to a Folder
   community.windows.win_gpo_manage:
-    mode: export
+    mode: exported
     folder: C:\gpoexport
 
 - name: Delete specific GroupPolicy
   community.windows.win_gpo_manage:
-    mode: remove
+    mode: absent
     name: '"GPO-1","GPO-2"'
-
-- name: Query specific GroupPolicy
-  community.windows.win_gpo_manage:
-    mode: query
 
 '''
 
