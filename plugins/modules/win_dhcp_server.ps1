ad5450460457485cee10bd095b6898d4e0378dbc
sgruber <12892285+sgruber94@users.noreply.github.com>
2021-04-08 13:06:15 +0200

    added requested changes

--- plugins/modules/win_dhcp_server.ps1
+++ plugins/modules/win_dhcp_server.ps1
@@ -5,19 +5,19 @@
 #AnsibleRequires -CSharpUtil Ansible.Basic
 $spec = @{
     options             = @{
-        type              = @{ type = "str"; choices = "scope", "exclusion"; required =$true }
-        version           = @{ type = "str"; choices = "IPv4", "IPv6"; default = "IPv4" }
-        scope             = @{ type = "str"; aliases = "prefix" }
-        name              = @{ type = "str"; required =$true  }
-        validlifetime     = @{ type = "str" ; default = "0.04:00:00" }
-        preferredlifetime = @{ type = "str" ; default = "0.02:00:00" }
-        startrange        = @{ type = "str" }
-        endrange          = @{ type = "str" }
-        subnetmask        = @{ type = "str" }
-        state             = @{ type = "str"; choices = "absent", "present"; default = "present" }
-        computername      = @{ type = "str" }
-        force             = @{ type = "bool"; default = $false }
-        scopestate        = @{ type = "str"; choices = "Active", "InActive"; default = "Active" }
+        type               = @{ type = "str"; choices = "scope", "exclusion"; required = $true }
+        version            = @{ type = "str"; choices = "IPv4", "IPv6"; default = "IPv4" }
+        scope              = @{ type = "str"; aliases = "prefix" }
+        name               = @{ type = "str"; required = $true }
+        valid_lifetime     = @{ type = "str" ; default = "0.04:00:00" }
+        preferred_lifetime = @{ type = "str" ; default = "0.02:00:00" }
+        start_range        = @{ type = "str" }
+        end_range          = @{ type = "str" }
+        subnet_mask        = @{ type = "str" }
+        state              = @{ type = "str"; choices = "absent", "present"; default = "present" }
+        dhcp_server        = @{ type = "str" }
+        force              = @{ type = "bool"; default = $false }
+        scope_state        = @{ type = "str"; choices = "Active", "InActive"; default = "Active" }
     }
     supports_check_mode = $true
 }
@@ -39,41 +39,36 @@ $force = $module.Params.force #optional for forcing Parameter
 [System.TimeSpan]$preferredlifetime = $module.Params.preferredlifetime #needed for IPv6 Scope
 $subnetmask = $module.Params.subnetmask #needed for IPv4 Scope
 $extra_param = @{}
-if($computername) { $extra_param.ComputerName = $computername }
+if ($computername) { $extra_param.ComputerName = $computername }
 
 #ToDo: Need to validate each Param for types
 $varsnotcorrect = $true
-if($type -eq "scope" -and $version -eq "IPv6" -and $null -ne $scope_id -and $null -ne $preferredlifetime -and $null -ne $validlifetime) {
+if ($type -eq "scope" -and $version -eq "IPv6" -and $null -ne $scope_id -and $null -ne $preferredlifetime -and $null -ne $validlifetime) {
     #type scope and IPV6
     $varsnotcorrect = $false
-    if($preferredlifetime -gt $validlifetime) {
+    if ($preferredlifetime -gt $validlifetime) {
         $varsnotcorrect = $true
         $msg = "The Preferred LifeTime needs to be less or equal ValidLifetime"
     }
 }
-if($type -eq "scope" -and $version -eq "IPv4" -and $null -ne $scope_id -and $null -ne $subnetmask -and $null -ne $startrange) {
+if ($type -eq "scope" -and $version -eq "IPv4" -and $null -ne $scope_id -and $null -ne $subnetmask -and $null -ne $startrange) {
     #type scope and IPV6
     #nothing to do here
     $varsnotcorrect = $false
 }
-if($varsnotcorrect) {
+if ($varsnotcorrect) {
     #Exit Module when Vars not correctly specified
     $module.FailJson("Please check your Variables. $msg")
 }
 
-Try {
-    # Import DHCP Server PS Module
-    Import-Module DhcpServer
-} Catch {
-    # Couldn't load the DhcpServer Module
-    $module.FailJson("The DhcpServer module failed to load properly: $($_.Exception.Message)", $_)
-}
+# Import DHCP Server PS Module
+Import-Module DhcpServer
 # State: Absent
 if ($state -eq "absent") {
     #Remove scope
     if ($type -eq "scope") {
         $current_scope_exists = $false
-        if($version -eq "IPv4") {
+        if ($version -eq "IPv4") {
             $dhcpscopes = Get-DhcpServerv4Scope @extra_param
             $current_scope = $dhcpscopes | Where-Object { $_.ScopeId -eq $scope_id } | Select-Object *
             if ($current_scope) { $current_scope_exists = $true }
@@ -82,34 +77,24 @@ if ($state -eq "absent") {
             $current_scope = Get-DhcpServerv6Scope -Prefix $scope_id @extra_param
             if ($current_scope.Prefix -contains $scope_id) { $current_scope_exists = $true }
         }
-        if($check_mode) { $extra_param.WhatIf = $check_mode }
-        if($force) { $extra_param.Force = $force }
+        if ($check_mode) { $extra_param.WhatIf = $check_mode }
+        if ($force) { $extra_param.Force = $force }
         if ($current_scope_exists -eq $false) {
             $module.Result.msg = "The scope doesn't exist."
         } else {
             # If the scope exists, we need to destroy it
             if ($version -eq "IPv4") {
-                try {
-                    Remove-DhcpServerv4Scope -ScopeId $scope_id @extra_param
-                    $module.Result.changed = $true
-                } catch {
-                    $module.Result.changed = $false
-                    $module.FailJson("Unable to remove scope : $($_.Exception.Message)")
-                }
+                Remove-DhcpServerv4Scope -ScopeId $scope_id @extra_param
+                $module.Result.changed = $true
             } else {
                 #IPv6
-                try {
-                    Remove-DhcpServerv6Scope -Prefix $scope_id @extra_param
-                    $module.Result.changed = $true
-                } catch {
-                    $module.Result.changed = $false
-                    $module.FailJson("Unable to remove scope : $($_.Exception.Message)")
-                }
+                Remove-DhcpServerv6Scope -Prefix $scope_id @extra_param
+                $module.Result.changed = $true
             }
         }
-    } elseif($type -eq "exclusion") {
+    } elseif ($type -eq "exclusion") {
         $current_exclusion_exists = $false
-        if($version -eq "IPv4") {
+        if ($version -eq "IPv4") {
             $current_exclusion = Get-DhcpServerv4ExclusionRange -ScopeId $scope_id
             if (($current_exclusion.ScopeId -contains $scope_id) -and ($current_scope.StartRange -contains $startrange) -and ($current_scope.EndRange -contains $endrange)) {
                 $current_exclusion_exists = $true
@@ -121,8 +106,8 @@ if ($state -eq "absent") {
                 $current_exclusion_exists = $true
             }
         }
-        if($check_mode) { $extra_param.WhatIf = $check_mode }
-        if($force) { $extra_param.Force = $force }
+        if ($check_mode) { $extra_param.WhatIf = $check_mode }
+        if ($force) { $extra_param.Force = $force }
         #Destroy all the exclusions
         if ($current_exclusion_exists -eq $false) {
             $module.Result.msg = "The exclusion doesn't exist."
@@ -132,7 +117,7 @@ if ($state -eq "absent") {
                 StartRange = $startrange
                 EndRange   = $endrange
             }
-            if($check_mode) { $extra_param.WhatIf = $check_mode }
+            if ($check_mode) { $extra_param.WhatIf = $check_mode }
             if ($version -eq "IPv4") {
                 try {
                     Remove-DhcpServerv4ExclusionRange -ScopeId $scope_id @exclusion_param @extra_param
@@ -161,7 +146,7 @@ if ($state -eq "absent") {
 if ($state -eq "present") {
     #creating /edit scope
     if ($type -eq "scope") {
-        if($version -eq "IPv4") {
+        if ($version -eq "IPv4") {
             $dhcpscopes = Get-DhcpServerv4Scope @extra_param
             $current_scope = $dhcpscopes | Where-Object { $_.ScopeId -eq $scope_id } | Select-Object *
         } else {
@@ -177,30 +162,20 @@ if ($state -eq "present") {
         if ($current_scope_exists -eq $false) {
 
             $extra_param.state = $scopestate
-            if($name) { $extra_param.name = $name }
+            if ($name) { $extra_param.name = $name }
             if ($version -eq "IPv4") {
                 #Scope does not exist we need to create it
                 $extra_param.StartRange = $startrange
                 $extra_param.EndRange = $endrange
                 $extra_param.SubnetMask = $subnetmask
-                try {
-                    Add-DhcpServerv4Scope @extra_param
-                    $module.Result.changed = $true
-                } catch {
-                    $module.Result.changed = $false
-                    $module.FailJson("Unable to add scope : $($_.Exception.Message)")
-                }
+                Add-DhcpServerv4Scope @extra_param
+                $module.Result.changed = $true
             } else {
                 #IPv6
-                if($validlifetime) { $extra_param.validlifetime = $validlifetime }
-                if($preferredlifetime) { $extra_param.preferredlifetime = $preferredlifetime }
-                try {
-                    Add-DhcpServerv6Scope -Prefix $scope_id @extra_param
-                    $module.Result.changed = $true
-                } catch {
-                    $module.Result.changed = $false
-                    $module.FailJson("Unable to add scope : $($_.Exception.Message)")
-                }
+                if ($validlifetime) { $extra_param.validlifetime = $validlifetime }
+                if ($preferredlifetime) { $extra_param.preferredlifetime = $preferredlifetime }
+                Add-DhcpServerv6Scope -Prefix $scope_id @extra_param
+                $module.Result.changed = $true
             }
         } else {
             # If the scope exist, we need to compare it
@@ -210,9 +185,9 @@ if ($state -eq "present") {
                 #IPv6
             }
         }
-    } elseif($type -eq "exclusion") {
+    } elseif ($type -eq "exclusion") {
         $current_exclusion_exists = $false
-        if($version -eq "IPv4") {
+        if ($version -eq "IPv4") {
             $current_exclusion = Get-DhcpServerv4ExclusionRange -ScopeId $scope_id
             if (($current_exclusion.ScopeId -contains $scope_id) -and ($current_scope.StartRange -contains $startrange) -and ($current_scope.EndRange -contains $endrange)) {
                 $current_exclusion_exists = $true
@@ -231,22 +206,13 @@ if ($state -eq "present") {
                 EndRange   = $endrange
             }
             if ($version -eq "IPv4") {
-                try {
-                    Add-DhcpServerv4ExclusionRange -ScopeId $scope_id @exclusion_param
-                    $module.Result.changed = $true
-                } catch {
-                    $module.Result.changed = $false
-                    $module.FailJson("unable to add exclusion : $($_.Exception.Message)")
-                }
+                Add-DhcpServerv4ExclusionRange -ScopeId $scope_id @exclusion_param
+                $module.Result.changed = $true
             } else {
                 #IPv6
-                try {
-                    Add-DhcpServerv6ExclusionRange -Prefix $scope_id @exclusion_param
-                    $module.Result.changed = $true
-                } catch {
-                    $module.Result.changed = $false
-                    $module.FailJson("unable to add exclusion : $($_.Exception.Message)")
-                }
+
+                Add-DhcpServerv6ExclusionRange -Prefix $scope_id @exclusion_param
+                $module.Result.changed = $true
             }
         } else {
             # If the exclusion exists, we need to compare it
--- plugins/modules/win_dhcp_server.py
+++ plugins/modules/win_dhcp_server.py
@@ -9,6 +9,7 @@ DOCUMENTATION = r'''
 module: win_dhcp_server
 short_description: Manage Windows DHCP Server
 author: Sebastian Gruber (@sgruber94)
+version_added: '1.4.0'
 description:
   - Manage Windows Server DHCP  (IPv4/IPv6)
   - Adds, Removes and Modifies DHCP Scopes and exclusions
@@ -45,21 +46,21 @@ options:
       - Required if l(type=scope), otherwise ignored.
     type: str
     aliases: [prefix]
-  validlifetime:
+  valid_lifetime:
     description:
       - defines valid Life Time for IPv6
       - This time span is equal to or greater than the Preferred Lifetime also assigned to the scope.
       - Required if l(version=IPv6) and l(type=scope), otherwise ignored.
     type: str
     default: "0.04:00:00"
-  preferredlifetime:
+  preferred_lifetime:
     description:
       - defines Preferred Life Time for IPv6
       - This time span is equal to or less than the valid lifetime also assigned to the scope.
       - Required if l(version=IPv6) and l(type=scope), otherwise ignored.
     type: str
     default: "0.02:00:00"
-  scopestate:
+  scope_state:
     description:
       - Specify if a scope is Active or Inactive
       - Required if l(type=scope), otherwise ignored.
@@ -71,19 +72,19 @@ options:
       - Specify Scope Name
     type: str
     required: true
-  computername:
+  dhcp_server:
     description:
-      - optional for specify another Computer
+      - optional for specify a DHCP Server
     type: str
-  startrange:
+  start_range:
     description:
       - optional, defines Startrange for IPv4 Addressspace
     type: str
-  endrange:
+  end_range:
     description:
       - optional, defines Endrange for IPv4 Addressspace
     type: str
-  subnetmask:
+  subnet_mask:
     description:
       - optional, defines Subnetmask for IPv4 Addressspace
     type: str
@@ -96,26 +97,26 @@ options:
 
 EXAMPLES = r'''
     - name: ADD | DHCP IPv4 Scope
-      win_dhcp_server:
+      community.windows.win_dhcp_server:
           state: present
           version: "IPv4"
           type: "scope"
           name: "DHCP - Test Scope - IPv4"
           scope: "192.168.222.0"
-          startrange: "192.168.222.1"
-          endrange: "192.168.222.250"
-          scopestate: InActive
-          subnetmask: "255.255.255.0"
+          start_range: "192.168.222.1"
+          end_range: "192.168.222.250"
+          scope_state: InActive
+          subnet_mask: "255.255.255.0"
 
     - name: ADD | DHCP IPv6 Scope
-      win_dhcp_server:
+      community.windows.win_dhcp_server:
           state: present
           version: "IPv6"
           type: "scope"
           name: "DHCP - Test Scope - IPv6"
           prefix: "2001:5cc1::"
-          validlifetime: "0.04:00:00"
-          preferredlifetime: "0.02:00:00"
+          valid_lifetime: "0.04:00:00"
+          preferred_lifetime: "0.02:00:00"
 '''
 
 RETURN = r'''
