fdc1c1c0cd564e30489883e2563263dbbb63bd7c
Jordan Borean <jborean93@gmail.com>
2021-06-19 11:04:28 +1000

    Keep whitespace consistent

--- plugins/modules/win_lineinfile.ps1
+++ plugins/modules/win_lineinfile.ps1
@@ -67,7 +67,7 @@ function Present($path, $regex, $line, $insertafter, $insertbefore, $create, $ba
 	# Note that we have to clean up the path because ansible wants to treat / and \ as
 	# interchangeable in windows pathnames, but .NET framework internals do not support that.
 	$cleanpath = $path.Replace("/", "\");
-    $endswithnewline = $null
+	$endswithnewline = $null
 
 	# Check if path exists. If it does not exist, either create it if create == "yes"
 	# was specified or fail with a reasonable error message.
@@ -77,7 +77,7 @@ function Present($path, $regex, $line, $insertafter, $insertbefore, $create, $ba
 		}
 		# Create new empty file, using the specified encoding to write correct BOM
 		[System.IO.File]::WriteAllLines($cleanpath, "", $encodingobj);
-        $endswithnewline = $false
+		$endswithnewline = $false
 	}
 
 	# Initialize result information
@@ -98,10 +98,10 @@ function Present($path, $regex, $line, $insertafter, $insertbefore, $create, $ba
 	}
 	Else {
 		$lines = [System.Collections.ArrayList] $before;
-        If ($null -eq $endswithnewline ) {
-            $alltext = [System.IO.File]::ReadAllText($cleanpath, $encodingobj);
-            $endswithnewline = (($alltext[-1] -eq "`n") -or ($alltext[-1] -eq "`r"))
-        }
+		If ($null -eq $endswithnewline ) {
+			$alltext = [System.IO.File]::ReadAllText($cleanpath, $encodingobj);
+			$endswithnewline = (($alltext[-1] -eq "`n") -or ($alltext[-1] -eq "`r"))
+		}
 	}
 
 	if ($diff_support) {
@@ -201,9 +201,9 @@ function Present($path, $regex, $line, $insertafter, $insertbefore, $create, $ba
 			$result.backup = $result.backup_file
 		}
 
-        if ($endswithnewline) {
-            $lines.Add("")
-        }
+		if ($endswithnewline) {
+			$lines.Add("")
+		}
 
 		$writelines_params = @{
 			outlines = $lines
@@ -251,10 +251,10 @@ function Absent($path, $regex, $line, $backup, $validate, $encodingobj, $linesep
 	}
 	Else {
 		$lines = [System.Collections.ArrayList] $before;
-        $alltext = [System.IO.File]::ReadAllText($cleanpath, $encodingobj);
-        If (($alltext[-1] -eq "`n") -or ($alltext[-1] -eq "`r")) {
-            $lines.Add("")
-        }
+		$alltext = [System.IO.File]::ReadAllText($cleanpath, $encodingobj);
+		If (($alltext[-1] -eq "`n") -or ($alltext[-1] -eq "`r")) {
+			$lines.Add("")
+		}
 	}
 
 	if ($diff_support) {
