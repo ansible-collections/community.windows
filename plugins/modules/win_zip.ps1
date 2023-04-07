7481034df80c9fb47f2c1689e1df129d240f2fb5
hiyokotaisa <thel.vadam2485@gmail.com>
2023-04-07 11:13:06 +0900

    Remove spaces for Sanity test

--- plugins/modules/win_zip.ps1
+++ plugins/modules/win_zip.ps1
@@ -53,7 +53,7 @@ catch {
 Function Compress-Zip($src, $dest) {
     # Disable using backslash for Zip path. This works for .NET 4.6.1 or later
     [System.AppContext]::SetSwitch('Switch.System.IO.Compression.ZipFile.UseBackslash', $false)
-    
+
     If (-not $module.CheckMode) {
         If (Test-Path -LiteralPath $src -PathType Container) {
             [System.IO.Compression.ZipFile]::CreateFromDirectory($src, $dest, $compressionLevel, (-not $srcWildcard), $encoding)
