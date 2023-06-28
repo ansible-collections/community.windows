1f70c6031556f93e6e2a7c312508b080df03522d
Timo.Crabbe@bctsoftware.com <Timo.Crabbe@bctsoftware.com>
2023-06-28 07:53:31 +0200

    Reverted fix from PR 482, updated changelog to reflect this

--- plugins/modules/win_xml.ps1
+++ plugins/modules/win_xml.ps1
@@ -191,14 +191,7 @@ if ($type -eq "element") {
             if ($node.get_NodeType() -eq "Document") {
                 $node = $node.get_DocumentElement()
             }
-
-            if ($node.ChildNodes.Count -eq 0) {
-                $elements = @($node)
-            }
-            else {
-                $elements = $node.get_ChildNodes()
-            }
-
+            $elements = $node.get_ChildNodes()
             [bool]$present = $false
             [bool]$changed = $false
             if ($elements.get_Count()) {
