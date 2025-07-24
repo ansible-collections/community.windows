0f27cff42980c4390d8bd62ecbd7df30bbe24944
Johannes Wolf <mail@jwolf.me>
2025-07-24 22:33:27 +0200

    win_psmodule: Match return of function with its name
    Find-LatestPsModule should act similarly to Get-PsModule, which returns an Object.
    This also fixes the check of the existing modules' version.

--- plugins/modules/win_psmodule.ps1
+++ plugins/modules/win_psmodule.ps1
@@ -358,15 +358,14 @@ Function Find-LatestPsModule {
 
         $ht = Add-DefinedParameter -Hashtable $ht -ParametersNames $ParametersNames
 
-        $LatestModule = Find-Module @ht
-        $LatestModuleVersion = $LatestModule.Version
+        $LatestModule = Find-Module @ht | Select-Object Version
     }
     catch [ System.Exception ] {
         $ErrorMessage = "Cant find the module $($Name): $($_.Exception.Message)"
         Fail-Json $result $ErrorMessage
     }
 
-    if ( $LatestModuleVersion.GetType().FullName -eq 'System.Object[]' ) {
+    if ( $LatestModule.Version.GetType().FullName -eq 'System.Object[]' ) {
         $ErrorMessage = -join @(
             "The module $Name exists in multiple repositories. Define a repository or the desired "
             "version of the module."
@@ -374,7 +373,7 @@ Function Find-LatestPsModule {
         Fail-Json $result $ErrorMessage
     }
 
-    $LatestModuleVersion
+    $LatestModule
 }
 
 # Check PowerShell version, fail if < 5.0 and required modules are not installed
@@ -476,15 +475,15 @@ elseif ( $state -eq "latest") {
         Credential = $repo_credential
     }
 
-    $LatestVersion = Find-LatestPsModule @ht
+    $LatestModule = Find-LatestPsModule @ht
 
     $ExistingModule = Get-PsModule $Name
 
-    if ( ($LatestVersion.Version -ne $ExistingModule.Version) -or $force ) {
+    if ( ($LatestModule.Version -ne $ExistingModule.Version) -or $force ) {
 
         $ht = @{
             Name = $Name
-            RequiredVersion = $LatestVersion
+            RequiredVersion = $LatestModule.Version
             Repository = $repo
             AllowClobber = $allow_clobber
             SkipPublisherCheck = $skip_publisher_check
